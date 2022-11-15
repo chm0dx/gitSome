a81dcffab11cc751516b473b4c133685b161531d
Jason Nickola <jason@pulsarsecurity.com>
2022-11-15 02:08:29 -0500

    Search public commits, issues, and users for emails belonging to a given domain

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # gitSome
 
-gitSome extracts email, name, and user information from GitHub repo commits.
+gitSome extract email addresses and other info from various GitHub sources
 
 ## Install
 
@@ -10,32 +10,34 @@ gitSome extracts email, name, and user information from GitHub repo commits.
 
 ## Use
 
-    usage: gitSome.py [-h] (-u USER | -r REPO | -t TOKEN) [-f] [-p PROXY] [-fp FIREPROX] [-j]
-                  [-e EXCLUDE]
+    usage: gitSome.py [-h] (-d DOMAIN | -u USER | -r REPO | -t TOKEN) [-f] [-p PROXY] [-fp FIREPROX] [-j] [-e EXCLUDE]
 
-    Pull emails, names, and usersnames from GitHub repo commits.
+    Extract email addresses and other info from various GitHub sources
 
     optional arguments:
       -h, --help            show this help message and exit
-      -u USER, --user USER  The name of a GitHub user or org account
-      -r REPO, --repo REPO  The name of a GitHub repo
+      -d DOMAIN, --domain DOMAIN
+                            Search public commits, issues, and users for emails belonging to the provied domain
+      -u USER, --user USER  Search repos of the provided GitHub user (or org) account
+      -r REPO, --repo REPO  Search the provied GitHub repo
       -t TOKEN, --token TOKEN
-                            Analyze the repos associated with a GitHub personal access token
+                            Search the repos associated with the provided GitHub personal access token
       -f, --forks           Include commits from forked repos
       -p PROXY, --proxy PROXY
                             Send requests through a web or SOCKS proxy
       -fp FIREPROX, --fireprox FIREPROX
                             Rewrite request URLs to use a FireProx endpoint
-      -j, --json            Return full json of email, name, & account (as opposed to just the plaintext email)
+      -j, --json            Return full json results (as opposed to just the plaintext email)
       -e EXCLUDE, --exclude EXCLUDE
                             The name of a repo or account to exclude
 
     Examples:
+      python3 gitSome.py -d hooli.com
       python3 gitSome.py -u orgName
       python3 gitSome.py -u userName -f
       python3 gitSome.py -r userName/repoName -p http://0.0.0.0:8080
       python3 gitSome.py -t github_pat_xxx -r excluded/repo -r excluded_account
-      python3 gitSome.py -u user -d hooli.com -fp fireprox_url
+      python3 gitSome.py -u user -fp fireprox_url
 
 ## Disclaimer
 
--- gitSome.py
+++ gitSome.py
@@ -1,7 +1,7 @@
 import argparse
 from argparse import RawTextHelpFormatter
 import requests
-import sys
+import re
 
 proxies = {"https":""}
 auth_token = None
@@ -91,39 +91,91 @@ def start_from_account(account,forks,proxy,token,fireprox,exclusions):
 	return sorted(results, key=lambda result: result["repo"], reverse=True)
 
 
+def start_from_domain(domain,proxy,fireprox):
+	headers["Accept"] = "application/vnd.github.text-match+json"
+	if fireprox:
+		global base_url
+		base_url = fireprox
+	proxies["https"] = proxy
+	results = []
+	page = 1
+	
+	for search_field in ["issues","commits","users"]:
+		while True:
+			search_url = f'{base_url}search/{search_field}?q="{domain}"&per_page=100&page={page}'
+			search_results = requests.get(search_url,proxies=proxies, headers=headers).json()
+
+			if search_results.get("message") and "API rate limit exceeded" in search_results.get("message"):
+				raise RateLimited()
+			if search_results.get("message") and "Not Found" in search_results.get("message"):
+				raise NotFound()
+
+			for search_result in search_results["items"]:
+				if search_field == "users":
+					user_page_url = f'{base_url}users/{search_result["login"]}'
+					user_page = requests.get(search_url,proxies=proxies, headers=headers).json()
+					for email in re.findall(rf"([A-Za-z0-9+.]+@[\w]*{domain})",match["fragment"]):
+							result = {"email":email,"source":search_result["html_url"]}
+							if result not in results:
+								results.append(result)
+
+				else:
+					for match in search_result["text_matches"]:
+						for email in re.findall(rf"([A-Za-z0-9+.]+@[\w]*{domain})",match["fragment"]):
+							result = {"email":email,"source":search_result["html_url"]}
+							if result not in results:
+								results.append(result)
+			if len(search_results) == 100:
+				page +1
+			else:
+				break
+
+	page = 1
+	return results
+
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(
-		description = "Pull emails, names, and usersnames from GitHub repo commits.",
+		description = "Extract email addresses and other info from various GitHub sources",
 		epilog = '''
 Examples:
+  python3 gitSome.py -d hooli.com
   python3 gitSome.py -u orgName
   python3 gitSome.py -u userName -f
   python3 gitSome.py -r userName/repoName -p http://0.0.0.0:8080
   python3 gitSome.py -t github_pat_xxx -r excluded/repo -r excluded_account
-  python3 gitSome.py -u user -d hooli.com -fp fireprox_url
+  python3 gitSome.py -u user -fp fireprox_url
+
 
 ''',
 		formatter_class=RawTextHelpFormatter)
 	reqs = parser.add_mutually_exclusive_group(required=True)
-	reqs.add_argument("-u","--user",help="The name of a GitHub user or org account")
-	reqs.add_argument("-r","--repo",help="The name of a GitHub repo")
-	reqs.add_argument("-t","--token",help="Analyze the repos associated with a GitHub personal access token")
+	reqs.add_argument("-d","--domain",help="Search public commits, issues, and users for emails belonging to the provied domain")
+	reqs.add_argument("-u","--user",help="Search repos of the provided GitHub user (or org) account")
+	reqs.add_argument("-r","--repo",help="Search the provied GitHub repo")
+	reqs.add_argument("-t","--token",help="Search the repos associated with the provided GitHub personal access token")
 	parser.add_argument("-f","--forks",help="Include commits from forked repos",action="store_true")
 	parser.add_argument("-p","--proxy",help="Send requests through a web or SOCKS proxy")
 	parser.add_argument("-fp","--fireprox",help="Rewrite request URLs to use a FireProx endpoint")
-	parser.add_argument("-j","--json",help="Return full json of email, name, & account (as opposed to just the plaintext email)", action="store_true")
+	parser.add_argument("-j","--json",help="Return full json results (as opposed to just the plaintext email)", action="store_true")
 	parser.add_argument("-e","--exclude",help="The name of a repo or account to exclude", action="append", default=[])
+	
 	args = parser.parse_args()
 
 	try:
 		if args.user or args.token:
 			results = start_from_account(args.user,args.forks,args.proxy,args.token,args.fireprox,args.exclude)
+		elif args.domain:
+			results = start_from_domain(args.domain,args.proxy,args.fireprox)
 		else:
 			results = start_from_repo(args.repo,args.proxy,args.fireprox)
-		if args.json:
+		
+		if len(results) == 0:
+			print("Didn't find anything")
+		elif args.json:
 			print(results)
 		else:
-			[print(result) for result in (set([result.get("email") for result in results if result.get("email")]))]
+			emails = [result for result in (set([result.get("email") for result in results if result.get("email")]))]
+			[print(email) for email in sorted(emails, key=lambda x: x.split("@")[-1])]
 	except RateLimited:
 		print("Rate Limited :(")
 	except NotFound:
